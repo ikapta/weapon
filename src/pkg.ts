66299f04c2bfa9a977d25eedaeae9f1d54a874d7
ikapta <kapta.fu@outlook.com>
2022-06-06 14:50:53 +0800

    remove annotation

--- src/pkg.ts
+++ src/pkg.ts
@@ -1,8 +1,4 @@
-// @ts-ignore
-import { read } from 'fsxx';
 import { createRequire } from 'node:module';
-
-// const { name, version } = await read.json`./package.json`;
 
 const { name, version } = createRequire(import.meta.url)('../package.json')
 
