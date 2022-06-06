23f45157c4d5b5595d3e56b99b2e7802f80a3086
ikapta <kapta.fu@outlook.com>
2022-06-06 14:50:14 +0800

    fix pkg path

--- src/pkg.ts
+++ src/pkg.ts
@@ -1,6 +1,9 @@
 // @ts-ignore
 import { read } from 'fsxx';
+import { createRequire } from 'node:module';
 
-const { name, version } = await read.json`package.json`;
+// const { name, version } = await read.json`./package.json`;
+
+const { name, version } = createRequire(import.meta.url)('../package.json')
 
 export { name, version };
