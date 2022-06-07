f69f42e910c9179e467314fd91120ccd93be2fab
ikapta <kapta.fu@outlook.com>
2022-06-07 12:56:18 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
 brew install volta
 
 volta install node
